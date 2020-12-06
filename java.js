5185e3570ec79b9cc85c47d5a991a67c639ed000
Caillaudfelix <74654899+Caillaudfelix@users.noreply.github.com>
2020-12-06 20:12:49 +0100

    Java 1

---
+++ java.js
@@ -0,0 +1,20 @@
+monstre1.onmouseover = function(){
+    document.getElementById("stats1").style.visibility = "visible"
+}
+monstre1.onmouseout = function(){
+    document.getElementById("stats1").style.visibility = "hidden"
+}
+  
+monstre2.onmouseover = function(){
+    document.getElementById("stats2").style.visibility = "visible"
+}
+monstre2.onmouseout = function(){
+    document.getElementById("stats2").style.visibility = "hidden"
+}
+
+monstre3.onmouseover = function(){
+    document.getElementById("stats3").style.visibility = "visible"
+}
+monstre3.onmouseout = function(){
+    document.getElementById("stats3").style.visibility = "hidden"
+}
