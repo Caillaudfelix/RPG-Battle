287959bf06a579e0b0b6e86b1bc7c7e01e73c3bc
Caillaudfelix <74654899+Caillaudfelix@users.noreply.github.com>
2020-12-08 19:23:39 +0100

    update java 2

--- java.js
+++ java.js
@@ -1,20 +1,30 @@
-monstre1.onmouseover = function(){
-    document.getElementById("stats1").style.visibility = "visible"
-}
-monstre1.onmouseout = function(){
-    document.getElementById("stats1").style.visibility = "hidden"
-}
-  
-monstre2.onmouseover = function(){
-    document.getElementById("stats2").style.visibility = "visible"
-}
-monstre2.onmouseout = function(){
-    document.getElementById("stats2").style.visibility = "hidden"
+battaqueK.onclick = function(){
+	attaquePerso = 25;
+	pv = document.getElementById("pvMonstre1").innerHTML;
+	pv = (pv - attaquePerso);
+	document.getElementById("pvMonstre1").innerHTML = pvMonstre1;
+	if (pvMonstre1 == 0){
+		document.getElementById("monstre1").style.visibility = "hidden";
+	}
 }
 
-monstre3.onmouseover = function(){
-    document.getElementById("stats3").style.visibility = "visible"
+document.getElementById("monstre1").onmouseover = function(){
+    document.getElementById("stats1").style.visibility = "visible";
 }
-monstre3.onmouseout = function(){
-    document.getElementById("stats3").style.visibility = "hidden"
+document.getElementById("monstre1").onmouseout = function(){
+    document.getElementById("stats1").style.visibility = "hidden";
+}
+  
+document.getElementById("monstre2").onmouseover = function(){
+    document.getElementById("stats2").style.visibility = "visible";
+}
+document.getElementById("monstre2").onmouseout = function(){
+    document.getElementById("stats2").style.visibility = "hidden";
+}
+
+document.getElementById("monstre3").onmouseover = function(){
+    document.getElementById("stats3").style.visibility = "visible";
+}
+document.getElementById("monstre3").onmouseout = function(){
+    document.getElementById("stats3").style.visibility = "hidden";
 }
